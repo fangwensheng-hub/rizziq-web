4b4d0eab991f227e1f8d6fd39196b2a29019ccbf
fangwensheng-hub <fangwensheng789@gmail.com>
2026-02-19 15:34:58 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,8 @@
+/* Minimal service worker for PWA installability on Android Chrome */
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
